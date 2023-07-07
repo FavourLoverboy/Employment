5e9fd1b1d0182c902a95cd2ffcd4ade17ed62999
Nwokamma Favour Ehio <favourehio2019@gmail.com>
2023-07-07 23:20:41 +0100

    admin login

--- admin.php
+++ admin.php
@@ -2,9 +2,8 @@
     include('includes/auth/header.php');
     echo "<title>Login | Admin - Nack </title>";
 
-    $e = $errEmail = $errPassword = '';
+    $errEmail = $errPassword = $e = '';
     $validate_email = true;
-    $resetPasswordLevel = $secObj->encryptURLParam('admins');
 
     if($_POST){
         extract($_POST);
@@ -20,7 +19,7 @@
 
         if($validate_email){
             // Checking for email
-            $tblquery = "SELECT * FROM admins WHERE email = :email";
+            $tblquery = "SELECT * FROM admin WHERE email = :email";
             $tblvalue = [
                 ':email' => htmlspecialchars($enc_email)
             ];
@@ -28,84 +27,44 @@
             if($select){
                 foreach($select as $data){
                     extract($data);
-                    $_SESSION['myID'] = $id;
                     $_SESSION['email'] = $email;
                     $_SESSION['password'] = $password;
-                    $_SESSION['img'] = $img;
-                    $_SESSION['status'] = $status;
                 }
                 if($_SESSION['password'] == $pwd){
-                    if($secObj->decryptURLParam($_SESSION['status']) == "1"){
-                        $_SESSION['level'] = 'admin';
-                        header('location: a/dashboard');
-                        echo "<script>  window.location='a/dashboard' </script>";
-                    }else{
-                        $errPassword = "your account has been disabled";
-                    }
+                    $_SESSION['level'] = 'admin';
+                    header('location: a/dashboard');
+                    echo "<script>  window.location='user/dashboard' </script>";
                 }else{
                     $errPassword = "incorrect password";
                 }
             }else{
-                $errEmail = "email don't exist";
+                $errEmail = "We apologize for any confusion caused. It appears that the email provided does not exist in our records";
             }
         }else{
-            $errEmail = 'invalid email';
+            $errEmail = '
+            We apologize for the inconvenience, but it seems that the email provided is invalid';
         }
     }
 ?>
 
     <div class="container">
-        <div class="left-box">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <!-- <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol> -->
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="assets/images/1.png" class="d-block w-100" alt="Image 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/images/2.jpg" class="d-block w-100" alt="Image 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/images/3.png" class="d-block w-100" alt="Image 3">
-                    </div>
-                </div>
-                <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a> -->
+        <h1>Admin</h1>
+        <form method="POST">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="enter email" value="<?php echo $e; ?>" required>
+                <span class="error"><?php echo $errEmail; ?></span>
             </div>
-        </div>
-        <div class="right-box">
-            <h2>SIGN IN</h2>
-            <form id="login-form" method="POST">
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Email" value="<?php echo $e; ?>" required>
-                    <span><?php echo $errEmail; ?></span>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Password" required>
-                    <span><?php echo $errPassword; ?></span>
-                </div>
-                <div class="form-group">
-                    <button type="submit">Login</button>
-                </div>
-                <div class="form-group forgot-password">
-                    <a href="forgotPassword.php?l=<?php echo $resetPasswordLevel; ?>">Forgot password?</a>
-                </div>
-                <!-- <div class="form-group register-link">
-                    Don't have an account? <a href="#">Register here</a>
-                </div> -->
-            </form>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="enter password" required>
+                <span class="error"><?php echo $errPassword; ?></span>
+            </div>
+            <button type="submit">Login</button>
+        </form>
+        <br>
+        <div class="button-group">
+            <a href="forgotPassword" class="forgot-password-link">Forgot Password?</a>
         </div>
     </div>
 
