ffda200715c680838c4a27935fe8414254e849a2
Nwokamma Favour Ehio <favourehio2019@gmail.com>
2023-07-13 21:45:16 +0100

    forgot password

--- forgotPassword.php
+++ forgotPassword.php
@@ -1,24 +1,9 @@
 <?php 
     include('includes/auth/header.php');
-    echo "<title>Reset | Admin - Nack </title>";
+    echo "<title>Login | Admin - Nack </title>";
 
-    $e = $errEmail = '';
+    $errEmail = $text_success = $e = '';
     $validate_email = true;
-    if(isset($_GET['l'])){
-        $level = $secObj->decryptURLParam($_GET['l']);
-        $levels = ['admins', 'referral', 'users'];
-        if(!in_array($level, $levels)){
-            echo "<script>  window.location='login' </script>";
-        }elseif($level == $levels[0]){
-            $userLoginLevel = 'admin';
-        }elseif($level == $levels[1]){
-            $userLoginLevel = 'referral';
-        }else{
-            $userLoginLevel = 'login';
-        }
-    }else{
-        echo "<script>  window.location='login' </script>";
-    }
 
     if($_POST){
         extract($_POST);
@@ -33,65 +18,41 @@
 
         if($validate_email){
             // Checking for email
-            $tblquery = "SELECT * FROM $level WHERE email = :email";
+            $tblquery = "SELECT * FROM users WHERE email = :email";
             $tblvalue = [
                 ':email' => htmlspecialchars($enc_email)
             ];
             $select = $dbObj->tbl_select($tblquery, $tblvalue);
-            $errEmail = ($select) ? "we sent a reset link to your email" : "email don't exist";
+            if($select){
+                $errEmail = "We have dispatched a password reset link to your registered email address.";
+                $text_success = "text-success";
+            }else{
+                $errEmail = "We apologize for any confusion caused. It appears that the email provided does not exist in our records";
+            }
         }else{
-            $errEmail = 'invalid email';
+            $errEmail = '
+            We apologize for the inconvenience, but it seems that the email provided is invalid';
         }
     }
 ?>
 
     <div class="container">
-        <div class="left-box">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <!-- <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol> -->
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="assets/images/1.png" class="d-block w-100" alt="Image 1">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/images/2.jpg" class="d-block w-100" alt="Image 2">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="assets/images/3.png" class="d-block w-100" alt="Image 3">
-                    </div>
-                </div>
-                <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a> -->
+        <h1>Forgot Password</h1>
+        <form method="POST">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="enter email" value="<?php echo $e; ?>" required>
+                <span class="error <?php echo $text_success; ?>"><?php echo $errEmail; ?></span>
             </div>
+            <button type="submit">Proceed</button>
+        </form>
+        <br>
+        <div class="button-group">
+            <a href="registration_personal" class="login-link">Register?</a>
+            <a href="login" class="forgot-password-link">Login?</a>
         </div>
-        <div class="right-box">
-            <h2>Reset Password</h2>
-            <form id="login-form" method="POST">
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Email" value="<?php echo $e; ?>" required>
-                    <span><?php echo $errEmail; ?></span>
-                </div>
-                <div class="form-group">
-                    <button type="submit">Request</button>
-                </div>
-                <div class="form-group forgot-password">
-                    <a href="<?php echo $userLoginLevel; ?>">Login</a>
-                </div>
-                <!-- <div class="form-group register-link">
-                    Don't have an account? <a href="#">Register here</a>
-                </div> -->
-            </form>
+        <div class="button-group">
+            <a href="#" class="login-link">Home</a>
         </div>
     </div>
 
